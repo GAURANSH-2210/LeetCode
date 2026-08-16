52f63a14ed626a16dea1491eb18d7da91b4f0e4c
GAURANSH <gauranshtanwar12@gmail.com>
2026-08-16 23:33:56 +0530

    Time: 43 ms (24.51%), Space: 14.2 MB (73.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> solutionArray(2);
         for(int i=0; i<nums.size(); i++){
             for(int j=i+1; j<nums.size(); j++){
                 if(nums[i] + nums[j] == target){
-                    solutionArray[0] = i;
-                    solutionArray[1] = j;
-                    return solutionArray;
+                    return {i, j};
                 }
             }
         }
